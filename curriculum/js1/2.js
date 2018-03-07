7b353194fcf815065f8af20fe44c536029f28b9f
Peter Chon <PeteC@garagescript.org>
2018-03-06 22:33:44 -0800

    Solves problem 2 with code indented

--- curriculum/js1/2.js
+++ curriculum/js1/2.js
@@ -8,12 +8,12 @@
 
 const solution = (input1, input2, input3) => {
   if ((input1 >= input2) && (input1 >= input3)) {
-    return input1;
+  return input1;
   }
   if (input2 > input3) {
-    return input2;
+  return input2;
   }
-    return input3;
+  return input3;
 };
 
 module.exports = {
