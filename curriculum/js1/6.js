56e58522869095d0e9c6efc9a0746e2b7a42fa03
Peter Chon <PeteC@garagescript.org>
2018-06-06 22:14:49 -0700

    Changes suggested by Daniel incorporated into problem 6 solution

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,15 +5,10 @@
  * @returns {number}
  */
 
-const solution = (a, possibleDivisor = a -1, sum = 0) => {
-  if (possibleDivisor === 1) {
-    return sum;
-  }
-  if (a % possibleDivisor === 0) {
-    sum = sum + possibleDivisor;
-  }
-  possibleDivisor = possibleDivisor - 1;
-  return solution(a, possibleDivisor, sum);
+const solution = (a, possibleDivisor = a - 1, sum = 0) => {
+  if (possibleDivisor === 1) return sum;
+  if (a % possibleDivisor === 0) sum += possibleDivisor;
+  return solution(a, possibleDivisor - 1, sum);
 };
 
 module.exports = {
