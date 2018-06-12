325403fc4544b4f64d7dce2bcfbc3cd3c9876eea
Peter Chon <PeteC@garagescript.org>
2018-06-12 16:31:37 -0700

    solves js2 problem 2

--- curriculum/js2/2.js
+++ curriculum/js2/2.js
@@ -5,8 +5,10 @@
  * @returns {array}
  */
 
-const solution = (a) => {
-  return [];
+const solution = (a, loopCount = a.length - 1) => {
+  if (loopCount < 0) return a;
+  if (a[loopCount] % 2 != 0) a[loopCount] = 0;
+  return solution(a, loopCount - 1);
 };
 
 module.exports = {
